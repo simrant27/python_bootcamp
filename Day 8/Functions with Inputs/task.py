f581dce775a35569428dd66adfa4285ee2afa490
Simran <simrant009@gmail.com>
2024-10-17 01:27:01 +0545

    day 8

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello")
+    print("Namaste")
+    print("How are you?")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+
+greet_with_name("Simran")
---
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,13 @@
+# Functions with input
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+def greet_with(name, location):
+    print(f"Hell {name},\nWhat is it like in {location}")
+
+greet_with("Simran", "Pokhara")
